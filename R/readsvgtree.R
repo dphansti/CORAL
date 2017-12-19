8ff1ad6e7567d20b8540ed4acb3bec90d887d92f
dphansti <douglas_phanstiel@med.unc.edu>
2017-12-19 16:58:55 -0500

    working on extracting info from svg.

--- R/readsvgtree.R
+++ R/readsvgtree.R
@@ -1,3 +1,264 @@
+# Define a function that collects full lines from svg files
+cleansvg <- function(filename)
+{
+  # read in all lines
+  svgdata = readLines(filename)
+  svgdataclean = list()
+  curline = c()
+  
+  nlines = length(svgdata)
+  i = 1
+  j = 0
+  for (i  in 1:nlines)
+  {
+    # get line and strip tabs
+    line = svgdata[i]
+    line = gsub("\t","",line)
+    
+    # add to curent list
+    curline = paste(curline,line,sep="")
+    
+    if (endsWith(line,">") == TRUE || i == nlines)
+    {
+      j = j + 1
+      svgdataclean[[j]] = curline
+      curline = c()
+    }
+  }
+  return(svgdataclean)
+}
+
+#Define a function that extracts info from svg lines
+extractsvgline <- function(line,type="path")
+{
+  listofstuff = c()
+  if (type == "path")
+  {
+    trimline = gsub(c("<path |/>"),"",line)
+  }
+  
+  if (type == "circle")
+  {
+    trimline = gsub(c("<circle |/>"),"",line)
+  }
+  
+  if (type == "text")
+  {
+    uniprotinfo = strsplit(line,split = ">")[[1]][1]
+    uniprotinfo = strsplit(uniprotinfo,split = "://www.uniprot.org/uniprot/")[[1]][2]
+    uniprotinfo = gsub("\"","",uniprotinfo)
+    listofstuff = c(listofstuff,uniprotinfo)
+    names(listofstuff)[length(listofstuff)] = "uniprot"
+    
+    trimline = strsplit(line,split = ">")[[1]][2]
+    trimline = gsub(c("<text "),"",trimline)
+    text.label = strsplit(strsplit(line,split = ">")[[1]][3],"<")[[1]][1]
+    listofstuff = c(listofstuff,text.label)
+    names(listofstuff)[length(listofstuff)] = "text.label"
+  }  
+  
+  subline = unlist(strsplit(trimline," "))
+    
+  for (subin in subline)
+  {
+    items = unlist(strsplit(subin,"="))
+    value = items[2]
+    value =gsub("\"","",value)
+    listofstuff = c(listofstuff,value)
+    names(listofstuff)[length(listofstuff)] = items[1]
+  }
+  
+  return(listofstuff)
+}
+
+# Define a function that extracts svg info
+extractinfo <- function(cleansvgdata)
+{
+  svginfo = list()
+  svginfo$header = c()
+  section = "header"
+
+  # determine which items to read in  
+  id.kinrich.branch = c()
+  branch.coords = c()
+  
+  id.kinrich.text = c()
+  text.x = c()
+  text.y = c()
+  text.label = c()
+  uniprot = c()
+  
+  id.kinrich.node= c()
+  node.x = c()
+  node.y = c()
+  
+  # determine section
+  for (i in 1:length(cleansvgdata))
+  {
+    line = cleansvgdata[[i]]
+    if (line == "<g id=\"BRANCHES\">")
+    {
+      section = "branches"
+      next
+    }
+    if (line == "<g id=\"LABELS\">")
+    {
+      section = "labels"
+      next
+    }
+    if (line == "<g id=\"CIRCLES\">")
+    {
+      section = "nodes"
+      next
+    }
+    if (line == "<g id=\"GROUPS\">")
+    {
+      section = "groups"
+      next
+    }   
+    
+    if (line  ==  "</g>" || line  ==  "</svg>")
+    {
+      next
+    }
+    
+    # add header lines to print out later
+    if (section == "header")
+    {
+      svginfo$header = c(svginfo$header,line)
+    }
+    
+    # parse branches
+    if (section == "branches")
+    {
+      lineitems = extractsvgline(line,type="path")
+      
+      longidsplit = unlist(strsplit(lineitems["id"],"_"))
+      id= paste(longidsplit[3:length(longidsplit)],collapse="_")
+      id = gsub("\"","",id)
+      id.kinrich.branch = c(id.kinrich.branch,id)
+     
+      branch.coords = c(branch.coords,lineitems["d"])
+    }
+    
+    # parse labels
+    if (section == "labels")
+    {
+      lineitems = extractsvgline(line,type="text")
+      
+      longidsplit = unlist(strsplit(lineitems["id"],"_"))
+      id= paste(longidsplit[3:length(longidsplit)],collapse="_")
+      id = gsub("\"","",id)
+      id.kinrich.text = c(id.kinrich.text,id)
+      
+      text.x = c(text.x,lineitems["x"])
+      text.y = c(text.y,lineitems["y"])
+      text.label = c(text.label,lineitems["text.label"])
+      uniprot = c(uniprot,lineitems["uniprot"])
+    }
+      
+    # parse labels
+    if (section == "nodes")
+    {
+      lineitems = extractsvgline(line,type="circle")
+      
+      longidsplit = unlist(strsplit(lineitems["id"],"_"))
+      id= paste(longidsplit[3:length(longidsplit)],collapse="_")
+      id = gsub("\"","",id)
+      id.kinrich.node = c(id.kinrich.node,id)
+    
+      node.x = c(node.x,lineitems["cx"])
+      node.y = c(node.y,lineitems["cy"])
+    }
+    
+    # add header lines to print out later
+    if (section == "groups")
+    {
+      svginfo$groups = c(svginfo$groups,line)
+    }
+  }
+  
+  # build data frames
+  branches = data.frame(id.kinrich=id.kinrich.branch, branch.coords=branch.coords,stringsAsFactors = FALSE)
+  branches = branches[order(branches$id.kinrich),]
+  
+  labels   = data.frame(id.kinrich=id.kinrich.text,
+                        uniprot=uniprot,
+                        text.x=text.x,
+                        text.y=text.y,
+                        text.label=text.label,stringsAsFactors = FALSE)
+  labels = labels[order(labels$id.kinrich),]
+  
+  nodes   = data.frame(id.kinrich=id.kinrich.text,
+                        node.x=node.x,
+                        node.y=node.y,stringsAsFactors = FALSE)
+  nodes = nodes[order(nodes$id.kinrich),]
+  
+  # merge data frames
+  both = merge(branches,labels,by = "id.kinrich")
+  all = merge(both,nodes,by = "id.kinrich")
+  
+  # perform sanity check
+  nrow(labels)
+  nrow(nodes)
+  nrow(branches)
+
+  # setdiff(branches$ids,labels$ids)
+  # setdiff(labels$ids,branches$ids)
+
+  svginfo$dataframe = all
+  
+  return(svginfo)
+}
+
+
+
+# Define a function that build a clean data frame from the kinase tree svg
+readsvgtree <- function(svgtree="~/Dropbox/Work/Projects/Ongoing/CORAL/Rpackage/CORAL/Data/basetree.svg")
+{
+
+  # read in and organize lines
+  cleansvgdata = cleansvg(svgtree)
+  
+  # extract the relevant info
+  svginfo = extractinfo(cleansvgdata)
+
+  # fix header
+  svginfo$header =  "<svg xmlns=\"http://www.w3.org/2000/svg\"
+  xmlns:xlink=\"http://www.w3.org/1999/xlink\" >"
+  
+  # read in kinmap file
+  kinmap = read.table("~/Dropbox/Work/Projects/Ongoing/Kinrich/CURRENT/Data/kinmaplabels.txt",header=F,sep="\t",quote="")
+  colnames(kinmap) = kinmap[1,]
+  kinmap = kinmap[2:nrow(kinmap),]
+  
+  colnames(kinmap) = c("label","ids","HGNC","name","group","family","subfamily","uniprot")
+  kinmap = kinmap[which(kinmap$group != "Atypical"),]
+  
+  # check for similarities with kinmap
+  setdiff(svginfo$dataframe$ids,kinmap$ids)
+  setdiff(kinmap$ids,svginfo$dataframe$ids)
+  
+  # merge with kinmap info
+  svginfo$dataframe = merge(svginfo$dataframe,kinmap[,2:ncol(kinmap)],by = "ids")
+  
+  # recolor
+  pal = colorRampPalette(c("firebrick4","firebrick2","gold","grey","deepskyblue2","dodgerblue2","dodgerblue4"))(100)
+  svginfo$dataframe$col_line = sample(pal,nrow(svginfo$dataframe),replace = TRUE)
+  
+  # write new tree
+  writekinasetree(svginfo)
+  
+  # add new conversion columns
+  svginfo$dataframe = conversioncolumn(df=svginfo$dataframe,convtable="~/Dropbox/Work/Projects/Ongoing/Kinrich/CURRENT/Mapping/uniprot2ensembl.txt",colname="ensembl")
+  svginfo$dataframe = conversioncolumn(df=svginfo$dataframe,convtable="~/Dropbox/Work/Projects/Ongoing/Kinrich/CURRENT/Mapping/uniprot2entrez.txt",colname="entrez")
+  
+  # write RDS file
+  svginfo$dataframe$col_line = "lightgrey"
+  saveRDS(svginfo,"~/Dropbox/Work/Projects/Ongoing/Kinrich/CURRENT/Tree//KinaseTree_Master.RDS")
+  
+}
+
 
 # make a well formatted svg data frame
 a = orig_svginfo$dataframe
@@ -13,18 +274,7 @@ for (i in 1:nrow(a))
   ys = c(ys,y)
 }
 
-# Define a function that build a clean data frame from the kinase tree svg
-readsvgtree <- function(svgtree="~/Dropbox/Work/Projects/Ongoing/CORAL/Rpackage/CORAL/Data/basetree.svg")
-{
-  
-  
-  
-  
-  
-  
-  
-  
-}
+
 
 gooddataframe = data.frame(
   
