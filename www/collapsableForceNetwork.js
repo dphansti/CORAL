a7c532c11ad8e44e0bb014a7cb1266405c01d539
mikelaff <mikelaff@email.unc.edu>
2018-01-30 17:25:13 -0500

    Improved layout, added text over nodes to Force Network

--- www/collapsableForceNetwork.js
+++ www/collapsableForceNetwork.js
@@ -12,111 +12,117 @@ binding.renderValue = function(el, data) {
   
   if(data!=null){
 
-    var width = 960*1.5,
-      height = 500*2,
-      root;
+   
+      var width = 960*1.7,
+          height = 500*2.35,
+          root;
 
-    var force = d3.layout.force()
-      .size([width, height])
-      .on("tick", tick);
+      var force = d3.layout.force()
+          .linkDistance(80)
+          .charge(-50)
+          .gravity(.001)
+          .size([width, height])
+          .on("tick", tick);
 
-    var svg = d3.select("#" + $(el).attr('id')).append("svg")
-      .attr("width", width)
-      .attr("height", height);
+      var svg = d3.select("#" + $(el).attr('id')).append("svg")
+          .attr("width", width)
+          .attr("height", height)
+          .call(d3.behavior.zoom().on("zoom", redraw));
 
-    var link = svg.selectAll(".link"),
-      node = svg.selectAll(".node");
+      var link = svg.selectAll(".link"),
+          node = svg.selectAll(".node");
 
-    d3.json("kinome_tree.json", function(error, json) {
-      if (error) throw error;
+      d3.json("kinome_tree.json", function(error, json) {
+        if (error) throw error;
 
-      root = json;
-      update();
-    });
+        root = json;
+        update();
+      });
 
-    function update() {
+      function update() {
         var nodes = flatten(root),
-        links = d3.layout.tree().links(nodes);
+            links = d3.layout.tree().links(nodes);
 
-      // Restart the force layout.
-      force
-        .nodes(nodes)
-        .links(links)
-        .start();
+        // Restart the force layout.
+        force
+            .nodes(nodes)
+            .links(links)
+            .start();
 
-      // Update the links…
-      link = link.data(links, function(d) { return d.target.id; });
+        // Update links.
+        link = link.data(links, function(d) { return d.target.id; });
 
-      // Exit any old links.
-      link.exit().remove();
+        link.exit().remove();
 
-      // Enter any new links.
-      link.enter().insert("line", ".node")
-        .attr("class", "link")
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+        link.enter().insert("line", ".node")
+            .attr("class", "link");
 
-      // Update the nodes…
-      node = node.data(nodes, function(d) { return d.id; }).style("fill", color);
+        // Update nodes.
+        node = node.data(nodes, function(d) { return d.id; });
 
-      // Exit any old nodes.
-      node.exit().remove();
+        node.exit().remove();
 
-      // Enter any new nodes.
-      node.enter().append("circle")
-       .attr("class", "node")
-       .attr("cx", function(d) { return d.x; })
-       .attr("cy", function(d) { return d.y; })
-       .attr("r", function(d) { return Math.sqrt(d.size) / 10 || 4.5; })
-       .style("fill", color)
-       .on("click", click)
-       .call(force.drag);
-    }
+        var nodeEnter = node.enter().append("g")
+            .attr("class", "node")
+            .on("click", click)
+            .call(force.drag);
 
-    function tick() {
-      link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+        nodeEnter.append("circle")
+            .attr("r", function(d) { return Math.sqrt(d.size) / 7 || 4.5; });
 
-      node.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-    }
+        nodeEnter.append("text")
+            .attr("dy", ".35em")
+            .text(function(d) { return d.name; });
 
-    // Color leaf nodes orange, and packages white or blue.
-    function color(d) {
-     return d._children ? "#3182bd" : d.children ? "#c6dbef" : "#fd8d3c";
-    }
+        node.select("circle")
+            .style("fill", color);
+      }
 
-    // Toggle children on click.
-    function click(d) {
-      if (!d3.event.defaultPrevented) {
+      function tick() {
+        link.attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
+
+        node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+      }
+
+      function color(d) {
+        return d._children ? "#3182bd" // collapsed package
+            : d.children ? "#c6dbef" // expanded package
+            : "#fd8d3c"; // leaf node
+      }
+
+      // Toggle children on click.
+      function click(d) {
+        if (d3.event.defaultPrevented) return; // ignore drag
         if (d.children) {
           d._children = d.children;
           d.children = null;
         } else {
-           d.children = d._children;
-           d._children = null;
+          d.children = d._children;
+          d._children = null;
         }
         update();
       }
-    }
 
-    // Returns a list of all nodes under the root.
-    function flatten(root) {
-      var nodes = [], i = 0;
+      // Returns a list of all nodes under the root.
+      function flatten(root) {
+        var nodes = [], i = 0;
 
-      function recurse(node) {
-        if (node.children) node.children.forEach(recurse);
-        if (!node.id) node.id = ++i;
-        nodes.push(node);
+        function recurse(node) {
+          if (node.children) node.children.forEach(recurse);
+          if (!node.id) node.id = ++i;
+          nodes.push(node);
+        }
+
+        recurse(root);
+        return nodes;
       }
 
-      recurse(root);
-      return nodes;
-    }
+      function redraw() {
+        console.log("here", d3.event.translate, d3.event.scale); svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
+      }
   //closing if statement
   }
 //closing binding  
