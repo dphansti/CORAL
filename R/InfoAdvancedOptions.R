4d783ae9de2a56eb5ea9ee238afdfbf3e0ca7273
Kathleen Metz <ksmetz@email.unc.edu>
2018-03-13 10:34:09 -0400

    Expanded InfoAdvancedOptions

--- R/InfoAdvancedOptions.R
+++ R/InfoAdvancedOptions.R
@@ -2,8 +2,80 @@ div(id="InfoAdvancedOptionsBox",
     
     box(width="100%",
         div(
-         h3 ("fasdfasdafds"),
-         "asdjfad sdj kf adsfjhsdf asdhdsdhkjfadshjkfa dsfad f"
+         h1 ("Title"),
+         "Select a title to be placed above the Tree plot."
+        ),
+        div(
+         h1 ("Font"),
+         "Change global font settings and kinase label settings."
+        ),
+        div(
+         h2 ("Choose Font"),
+         "Select a font to use for all kinase and family labels, and titles."
+        ),
+        div(
+         h2 ("Label Font Size"),
+         "Select a font size to use for kinase labels."
+        ),
+        div(
+         h2 ("Label Color"),
+         "Select a color to use for kinase labels."
+        ),
+        div(
+         h3 ("Single Color"),
+         "Select a single uniform color for all labels using the color selector."
+        ),
+        div(
+         h3 ("Same as Branch"),
+         "Color all kinase labels the same color as their branches."
+        ),
+        div(
+         h1 ("Node"),
+         "Change node stroke settings."
+        ),
+        div(
+         h2 ("Node stroke color scheme"),
+         "Select a method with which to assign node stroke colors."
+        ),
+        div(
+         h3 ("Uniform"),
+         "This option allows you to color every node stroke as the same uniform color."
+        ),
+        div(
+         h4 ("Node Stroke Color"),
+         "A color-selector used to change the color of all branches."
+        ),
+        div(
+         h3 ("Same as Node"),
+         "This option sets the color of each node stroke to the node color."
+        ),
+        div(
+         h3 ("Manual (?)"),
+         "This option allows you to color strokes of specific nodes of interest."
+        ),
+        div(
+         h4 ("Kinase Input Method ???"),
+         "Select the method you would like to use to identify kinases of interest."
+        ),
+        div(
+         h5 ("Select"),
+         "Scroll through a searchable list of all kinases available on the tree."
+        ),
+        div(
+         h5 ("Paste"),
+         "Submit a list of kinases. (do names matter? Format?)"
+        ),
+        div(
+         h4 ("Kinases"),
+         "Select or input your list of kinases to highlight."
+        ),
+        div(
+         h4 ("BG Color & Color"),
+         "Select colors for your selected and unselected kinases."
+        ),
+        div(
+         h4 ("(Other option for titles??)"),
+         "Change the legend labels for selected and unselected kinases."
         )
     ) # end box
 ) # end div
