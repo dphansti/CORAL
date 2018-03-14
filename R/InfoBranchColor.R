563b1a791dd90533e20e35b56468d548dc14de2d
dphansti <douglas_phanstiel@med.unc.edu>
2018-03-14 14:42:52 -0400

    Formatting of Info Pages

--- R/InfoBranchColor.R
+++ R/InfoBranchColor.R
@@ -1,104 +1,154 @@
 div(id="InfoBranchColorBox",
     box(width="100%",
+
+    fluidRow(width=12, column(8,   
         div(
-         h1 ("Color Scheme"),
+         h1 ("Branch Color"),
          "Select a method with which to assign branch colors."
-        ),
-        div(
-         h2 ("Uniform"),
-         "This option allows you to color every branch as a single uniform color. "
-        ),
-        div(
+    ))),
+    
+    fluidRow(width=12, column(8, 
+        div(       
+         h2 (HTML("Uniform &nbsp;&nbsp;▾")),
+         "This option allows you to color every branch as a single uniform color."
+         ))),
+         
+         fluidRow(width=12,
+                  column(2, tags$br(), div(img(src="info/BC-Uniform.png",width="100%",align="left",
+                            tags$style("img[src='info/BC-Uniform.png'] {padding-top: 4px}")
+                            ))),
+                  column(6,
+         div(       
          h3 ("Branch Color"),
-         "A color-selector used to change the color of all branches."
-        ),
+         "A color-selector used to change the color of all branches."))),
+    
+    fluidRow(width=12, column(8,     
         div(
-         h2 ("Manual"),
+         h2 (HTML("Manual &nbsp;&nbsp;▾")),
          "This option allows you to highlight branches to specific kinases of interest. "
-        ),
+        ))),
+        
+         fluidRow(width=12,
+                  column(2, tags$br(), div(img(src="info/BC-Manual.png",width="100%",align="left",
+                            tags$style("img[src='info/BC-Manual.png'] {padding-top: 4px}")                  
+                            ))),
+                  column(6,
+     
         div(
          h3 ("Kinase Input Method"),
-         "Select the method you would like to use to identify kinases of interest."
-        ),
-        div(
-         h4 ("Select"),
-         "Scroll through a searchable list of all kinases available on the tree. "
-        ),
-        div(
-         h4("Paste"),
-         "Submit a list of kinases. (do names matter? Format?)"
-        ),
-        div(
+         "Select the method you would like to use to identify kinases of interest.",
+         
+         h4 (HTML("☉&nbsp; Select")),
+         "Scroll through a searchable list of all kinases available on the tree. ",
+         
+         h4 (HTML("☉&nbsp; Paste")),
+         "Submit a list of kinases. (do names matter? Format?)",
+         
          h3 ("Kinases"),
-         "Select or input your list of kinases to highlight."
-        ),
-        div(
+         "Select or input your list of kinases to highlight.",
+         
          h3 ("BG Color & Color"),
-         "Select colors for your selected and unselected kinases."
-        ),
-        div(
+         "Select colors for your selected and unselected kinases.",
+         
          h3 ("(Other option for titles??)"),
-         "Change the legend labels for selected and unselected kinases."
-        ),
-        div(
+         "Change the legend labels for selected and unselected kinases.",
+         tags$br(),tags$br(),
          "This option will automatically create a figure legend indicating the colors of selected and not selected kinases."
-        ),
+        ))),
+
+    fluidRow(width=12, column(8,    
         div(
-         h2 ("Qualitative"),
+         h2 (HTML("Qualitative &nbsp;&nbsp;▾")),
          "This option allows you to color kinase branches of different colors, to differentiate between multiple categories."
-        ),
+        ))),
+        
+        fluidRow(width=12,
+                 column(2, tags$br(), div(img(src="info/BC-Qualitative.png",width="100%",align="left",
+                           tags$style("img[src='info/BC-Uniform.png'] {padding-top: 4px}")
+                 ))),
+                 column(6,
+     
         div(
-         h3 ("Load default kinase groups"),
-         "Load the default kinase groups as defined by Manning et al 2002 into the 'Kinases & Group' tab."
-        ),
-        div(
+         h4 (HTML("◎&nbsp; Load default kinase groups")),
+         "Load the default kinase groups as defined by Manning et al 2002 into the 'Kinases & Group' tab.",
+         
          h3 ("Kinases & Group"),
-         "Submit a two-column list of kinases and their group assignments." 
-        ),
-        div(
+         "Submit a two-column list of kinases and their group assignments.",
+         
          h3 ("Identifier Type"),
-         "Select the kinase identifier used to list kinases in the 'Kinases & Group' tab (coralID, uniprot, ensemble, entrez, HGNC)."
-        ),
-        div(
+         "Select the kinase identifier used to list kinases in the 'Kinases & Group' tab (coralID, uniprot, ensemble, entrez, HGNC).",
+         
          h3 ("Color Range Type"),
-         "Color groups based on pre-built color palettes, or manually assign colors."
-        ),
-        div(
+         "Color groups based on pre-built color palettes, or manually assign colors.",
+         
          h3 ("Manual Group Entry"),
-         "???????"
-        ),
-        div(
+         "???????",
+         tags$br(),tags$br(),
          "This option will automatically create a figure legend showing the color and group name for each category listed."
-        ),
+        ))),
+    
+    fluidRow(width=12, column(8, 
         div(
-         h2 ("Quantitative"),
+         h2 (HTML("Quantitative &nbsp;&nbsp;▾")),
          "This option allows you to color kinase branches based on a range of qualitative values"
-        ),
+        ))),
+        
+        fluidRow(width=12,
+                 column(2, tags$br(), div(img(src="info/BC-Quantitative.png",width="100%",align="left",
+                           tags$style("img[src='info/BC-Uniform.png'] {padding-top: 4px}")
+                 ))),
+                 
+                 column(6,        
+        
         div(
-         h3 ("Load example data"),
-         "Load sample data into the 'Kinases & Value' tab."
-        ),
-        div(
+         h4 (HTML("◎&nbsp; Load example data")),
+         "Load sample data into the 'Kinases & Value' tab.",
+         
          h3 ("Kinases & Value"),
-         "Submit a two-column list of kinases and their associated values."
-        ),
-        div(
+         "Submit a two-column list of kinases and their associated values.",
+         
          h3 ("Identifier Type"),
-         "Select the kinase identifier used to list kinases in the 'Kinases & Group' tab (coralID, uniprot, ensemble, entrez, HGNC)."
-        ),
-        div(
+         "Select the kinase identifier used to list kinases in the 'Kinases & Group' tab (coralID, uniprot, ensemble, entrez, HGNC).",
+         
          h3 ("Min & Max"),
-         "Indicate the values that will represent either end of the color spectrum. All values below the assigned min will be colored the same, as will all values above the assigned max."
-        ),
-        div(
+         "Indicate the values that will represent either end of the color spectrum. All values below the assigned min will be colored the same, as will all values above the assigned max.",
+         
          h3 ("Color Range Type"),
-         "Choose between pre-built sequential and divergent color palettes or create your own 2- or 3-color gradient using color selectors."
+         "Choose between pre-built sequential and divergent color palettes or create your own 2- or 3-color gradient using color selectors.",
+         tags$br(),tags$br(),
+         "This option will automatically create a figure legend showing the full spectrum of possible colors and indicating the low, middle, and high values."
+        ))),
+    
+        
+        div(
+         h1 ("H1"),
+         "abcdefg hij klmno pqrstu vwxyz"
         ),
         div(
-         "This option will automatically create a figure legend showing the full spectrum of possible colors and indicating the low, middle, and high values."
+         h2 ("H2"),
+         "abcdefg hij klmno pqrstu vwxyz"
+        ),
+        div(
+         h3 ("H3"),
+         "abcdefg hij klmno pqrstu vwxyz"
+        ),
+        div(
+         h4 ("H4"),
+         "abcdefg hij klmno pqrstu vwxyz"
+        ),
+        div(
+         h5 ("H5"),
+         "abcdefg hij klmno pqrstu vwxyz"
+        ),
+        div(
+         h6 ("H6"),
+         "abcdefg hij klmno pqrstu vwxyz"
         )
     ) # end box
 ) # end div
+
+
+
 
 
 
---
+++ R/Test.R
@@ -0,0 +1,48 @@
+div(id="InfoBranchColorBox",
+    box(width="100%",
+        div(
+         h1 ("Branch Color"),
+         "For questions or comments please post an issue on our github page or contact us directly at asdfadsf.med.unc.edu"
+        ),
+        div(
+         h2 ("Color Branch"),
+         "CORAL is distributed via safads asdf asdf sadf adsf"
+        ),
+        div(
+         h2 ("Color Branch"),
+         "CORAL is distributed via safads asdf asdf sadf adsf"
+        ),
+        div(
+         h3 (HTML("▾&nbsp;&nbsp; By Value")),
+         "For questions or comments please post an issue on our github page or contact us directly at asdfadsf.med.unc.edu"
+        ),
+        div(
+         h3 (HTML("◎&nbsp; Load Example Data")),
+         "CORAL is distributed via safads asdf asdf sadf adsf"
+        ),
+        div(
+         h3 (HTML("☉&nbsp; Ldgfds")),
+         "For questions or comments please post an issue on our github page or contact us directly at asdfadsf.med.unc.edu"
+        ),
+        div(
+         h2 ("sdfgsfdg"),
+         "CORAL is distributed via safads asdf asdf sadf adsf"
+        ),
+        div(
+         h3 ("sdfgsfdg"),
+         "CORAL is distributed via safads asdf asdf sadf adsf"
+        ),
+        div(
+         h4 ("sdfgsfdg"),
+         "CORAL is distributed via safads asdf asdf sadf adsf"
+        ),
+        div(
+         h5 ("sdfgsfdg"),
+         "CORAL is distributed via safads asdf asdf sadf adsf"
+        ),
+        div(
+         h6 ("sdfgsfdg"),
+         "CORAL is distributed via safads asdf asdf sadf adsf"
+        )
+    ) # end box
+) # end div
