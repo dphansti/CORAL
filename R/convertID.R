6dfad8e6795ddd12ac60744bd18a09e9b695be6a
dphansti <douglas_phanstiel@med.unc.edu>
2018-05-23 11:56:14 -0400

    Accounted for the case when on other ID (e.g. HGNC) corresponds to multiple coral IDs.  Now it assigns that value to all of the corresponding coral IDs.

--- R/convertID.R
+++ R/convertID.R
@@ -23,12 +23,29 @@ convertID <- function(df,recolordf,inputtype)
   # filter input df for those found in table
   recolordf = recolordf[which(recolordf[,1] %in% df[,inputtype]),]
 
-  # make lookup
-  values  = df[,1]
-  names(values) = df[,inputtype]
+  # Make a new recolordf accounting for the fact that one other ID could correspond
+  # to multilpe coral IDs
+  newids = c()
+  newvalues = c()
+  for (i in 1:nrow(recolordf))
+  {
+   otherid  = recolordf[i,1]
+   quantval = recolordf[i,2]
+   print (" ")
+   print (i)
+   print ("otherid")
+   print(otherid)
+   print ("quantval")
+   print(quantval)
+   print ("----")
+   coralids = as.character(df[which(as.character(df[,inputtype]) == otherid),1])
+   for (coralid in coralids)
+   {
+    newids = c(newids,coralid)
+    newvalues = c(newvalues,quantval)
+   }
+  }
+  recolordfconverted = data.frame(ids=newids,values=newvalues,stringsAsFactors = F)
   
-  # convert 
-  recolordfconverted = data.frame(ids = values[as.character(recolordf[,1])], values =recolordf[,2], stringsAsFactors = F)
-
   return(recolordfconverted)
 }
